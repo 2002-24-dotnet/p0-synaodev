9377e82a169f90d8748e0073319cc4c10f290d5c
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-03-05 07:17:25 -0600

    Finally working!

---
+++ PizzaBox.Storage/Migrations/20200305130231_Migrations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaBox.Storage.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pizzas",
+                column: "ID",
+                value: 1);
+
+            migrationBuilder.InsertData(
+                table: "Pizzas",
+                column: "ID",
+                value: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pizzas");
+        }
+    }
+}
